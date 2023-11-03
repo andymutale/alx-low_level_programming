4ef3f02d0edfca1c0493792917a95623d9c2429a
andymutale <drewmutale@gmail.com>
2023-11-02 17:17:13 -0700

    n

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - Concatenates two strings
+ * @s1: First string
+ * @s2: Second string
+ * @n: Number of bytes to concatenate from s2
+ *
+ * Return: Pointer to the newly allocated string, or NULL if memory allocation fails
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+      	char *result;
+       	unsigned int len1 = 0, len2 = 0, i;
+
+      	if (s1 != NULL)
+      	{
+		len1 = strlen(s1);
+      	}
+
+      	if (s2 != NULL)
+      	{
+	  	len2 = strlen(s2);
+      	}
+      
+	if (n >= len2)
+      	{
+	  	n = len2;
+	}
+	
+      	result = malloc((len1 + n + 1) * sizeof(char));
+      	if (result == NULL)
+      	{
+	  	return (NULL);
+      	}
+
+      	for (i = 0; i < len1; i++)
+      	{
+	  	result[i] = s1[i];
+      	}
+
+      	for (i = 0; i < n; i++)
+      	{
+	  	result[len1 + i] = s2[i];
+      	}
+
+      	result[len1 + n] = '\0';
+
+      	return (result);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void _putchar(char c);
 
 #endif /* MAIN_H */
 
