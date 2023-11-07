08fa23cf2a2eb1ec391bc2fc0fe2f52267be41ba
andymutale <drewmutale@gmail.com>
2023-11-06 16:56:00 -0800

    m

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
 
-/*
- * Struct for representing a dog.
+/**
+ * struct dog - Represents a dog with its name, age, and owner.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  */
 typedef struct dog
 {
---
+++ 0x0E-structures_typedef/free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees memory allocated for a struct dog
+ * @d: Pointer to the struct dog to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
+
