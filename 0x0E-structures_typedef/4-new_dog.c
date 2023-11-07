bc67a8369742c6fefc55c209b4fd1a021fa63797
andymutale <drewmutale@gmail.com>
2023-11-06 16:16:22 -0800

    s

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,35 +13,35 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog;
-    char *name_copy, *owner_copy;
+	dog_t *new_dog;
+	char *name_copy, *owner_copy;
 
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-        return (NULL);
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
 
-    name_copy = malloc(strlen(name) + 1);
-    if (name_copy == NULL)
-    {
-        free(new_dog);
-        return (NULL);
-    }
+	name_copy = malloc(strlen(name) + 1);
+	if (name_copy == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
 
-    owner_copy = malloc(strlen(owner) + 1);
-    if (owner_copy == NULL)
-    {
-        free(new_dog);
-        free(name_copy);
-        return (NULL);
-    }
+	owner_copy = malloc(strlen(owner) + 1);
+	if (owner_copy == NULL)
+	{
+		free(new_dog);
+		free(name_copy);
+		return (NULL);
+	}
 
-    strcpy(name_copy, name);
-    strcpy(owner_copy, owner);
+	strcpy(name_copy, name);
+	strcpy(owner_copy, owner);
 
-    new_dog->name = name_copy;
-    new_dog->age = age;
-    new_dog->owner = owner_copy;
+	new_dog->name = name_copy;
+	new_dog->age = age;
+	new_dog->owner = owner_copy;
 
-    return (new_dog);
+	return (new_dog);
 }
 
