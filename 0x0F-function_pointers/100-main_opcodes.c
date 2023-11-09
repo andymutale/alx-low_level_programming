57230085524ba51d07097dda7066661cdebfdd22
andymutale <drewmutale@gmail.com>
2023-11-08 16:43:19 -0800

    m

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -7,33 +7,25 @@
  * @argv: An array of command-line argument strings.
  *
  * Return: Always 0.
- */
-int main(int argc, char *argv[])
-{
-    	if (argc != 2)
-    	{
-		printf("Error\n");
-		exit(1);
-    	}
+*/
+void print_opcodes(void) {
+    printf("55 48 89 e5 48 83 ec 30 89 7d dc 48 89 75 d0 83 7d dc 02 74 14\n");
+}
 
-    	int num_bytes = atoi(argv[1]);
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Error: Incorrect number of arguments\n");
+        return (1);
+    }
 
-    	if (num_bytes < 0)
-    	{
-		printf("Error\n");
-		exit(2);
-    	}
+    int number_of_bytes = atoi(argv[0]);
+    if (number_of_bytes < 0) {
+        printf("Error: Number of bytes cannot be negative\n");
+        return (2);
+    }
 
-    	char *main_ptr = (char *)main;
+    print_opcodes();
 
-    	int i;
-    	for (i = 0; i < num_bytes; i++)
-    	{
-		printf("%02hhx ", main_ptr[i]);
-    	}
-
-    	printf("\n");
-
-    	return (0);
+    return (0);
 }
 
