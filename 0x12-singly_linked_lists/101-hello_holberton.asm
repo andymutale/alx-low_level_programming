c0af46781acff2048e9b20b67acd8a38c28ce11a
andy <91167584+andymutale@users.noreply.github.com>
2023-11-17 04:18:43 +0200

    Create 101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,13 @@
+section .data
+    hello db "Hello, Holberton", 0
+
+section .text
+    global main
+    extern printf
+
+main:
+    push rbp
+    mov rdi, hello
+    call printf
+    pop rbp
+    ret
