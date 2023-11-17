6a9ab50adbaef27547551b5514024a111061ae7e
andymutale <drewmutale@gmail.com>
2023-11-16 20:49:11 -0800

    m

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -12,5 +12,7 @@ void my_startup_function(void) __attribute__ ((constructor));
  */
 void my_startup_function(void)
 {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
 }
+
