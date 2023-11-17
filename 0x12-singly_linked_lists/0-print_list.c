ea0d4dad8d0b4641cd871c2a746357717245ec09
andymutale <drewmutale@gmail.com>
2023-11-16 18:43:10 -0800

    m

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,18 +10,18 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t node_count = 0;
+    	size_t node_count = 0;
 
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", h->len, h->str);
+    	while (h != NULL)
+    	{
+		if (h->str == NULL)
+	    		printf("[0] (nil)\n");
+		else
+	    		printf("[%u] %s\n", h->len, h->str);
 
-        node_count++;
-        h = h->next;
-    }
+		node_count++;
+		h = h->next;
+    	}
 
-    return node_count;
+    	return (node_count);
 }
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,37 +11,38 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node, *temp;
+    	list_t *new_node, *temp;
 
-    if (head == NULL || str == NULL)
-        return (NULL);
+    	if (head == NULL || str == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+    	new_node = malloc(sizeof(list_t));
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->len = strlen(str);
-    new_node->next = NULL;
+    	new_node->str = strdup(str);
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-    }
-    else
-    {
-        temp = *head;
-        while (temp->next != NULL)
-            temp = temp->next;
+    	if (new_node->str == NULL)
+    	{
+		free(new_node);
+		return (NULL);
+    	}
 
-        temp->next = new_node;
-    }
+    	new_node->len = strlen(str);
+    	new_node->next = NULL;
 
-    return new_node;
+    	if (*head == NULL)
+    	{
+		*head = new_node;
+    	}
+    	else
+    	{
+		temp = *head;
+		while (temp->next != NULL)
+	    		temp = temp->next;
+
+		temp->next = new_node;
+    	}
+    	return (new_node);
 }
