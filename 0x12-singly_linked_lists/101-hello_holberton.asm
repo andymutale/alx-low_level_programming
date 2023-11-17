152bfe4396a596123b6b3fce9859f5fafc898f34
andy <91167584+andymutale@users.noreply.github.com>
2023-11-17 05:05:42 +0200

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,19 @@
 section .data
-    hello db "Hello, Holberton", 0
+   hello db 'Hello, Holberton', 0
 
 section .text
-    global main
-    extern printf
+   global main
+   extern printf
 
 main:
-    push rbp
-    mov rdi, hello
-    call printf
-    pop rbp
-    ret
+   ; Prepare arguments for printf
+   mov rdi, hello ; First argument is the format string
+   xor rax, rax ; Number of floating point parameters
+
+   ; Call printf
+   call printf
+
+   ; Exit program
+   mov eax, 0x60 ; System call number for exit
+   xor edi, edi ; Exit code
+   syscall
