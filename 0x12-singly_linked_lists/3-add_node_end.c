bf87bb686cbd5ac85ce0a424d2024117d11575fd
andy <91167584+andymutale@users.noreply.github.com>
2023-11-17 05:00:59 +0200

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,38 +11,37 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    	list_t *new_node, *temp;
+    list_t *new_node, *temp;
 
-    	if (head == NULL || str == NULL)
-		return (NULL);
+    if (head == NULL || str == NULL)
+        return (NULL);
 
-    	new_node = malloc(sizeof(list_t));
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return (NULL);
 
-	if (new_node == NULL)
-		return (NULL);
+    new_node->str = strdup(str);
+    if (new_node->str == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
 
-    	new_node->str = strdup(str);
+    new_node->len = strlen(str);
+    new_node->next = NULL;
 
-    	if (new_node->str == NULL)
-    	{
-		free(new_node);
-		return (NULL);
-    	}
+    if (*head == NULL)
+    {
+        *head = new_node;
+    }
+    else
+    {
+        temp = *head;
+        while (temp->next != NULL)
+            temp = temp->next;
 
-    	new_node->len = strlen(str);
-    	new_node->next = NULL;
+        temp->next = new_node;
+    }
 
-    	if (*head == NULL)
-    	{
-		*head = new_node;
-    	}
-    	else
-    	{
-		temp = *head;
-		while (temp->next != NULL)
-	    		temp = temp->next;
-
-		temp->next = new_node;
-    	}
-    	return (new_node);
+    return (new_node);
 }
