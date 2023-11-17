fc4c34dfea460fee0b1a809fc4815f2527a7e9a0
andymutale <drewmutale@gmail.com>
2023-11-16 21:10:52 -0800

    m

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,18 @@
 section .data
-   hello db 'Hello, Holberton', 0
+    hello db 'Hello, Holberton', 0
 
 section .text
-   global main
-   extern printf
+    global main
+    extern printf
 
 main:
-   ; Prepare arguments for printf
-   mov rdi, hello ; First argument is the format string
-   xor rax, rax ; Number of floating point parameters
+    ; Prepare the arguments for printf
+    mov rdi, hello     ; The format string
+    xor rax, rax       ; 0 floating point parameters
+    ; Call printf
+    call printf
+    ; Return 0
+    mov eax, 0x60      ; The syscall number for sys_exit
+    xor edi, edi       ; Exit code 0
+    syscall            ; Call the kernel
 
-   ; Call printf
-   call printf
-
-   ; Exit program
-   mov eax, 0x60 ; System call number for exit
-   xor edi, edi ; Exit code
-   syscall
