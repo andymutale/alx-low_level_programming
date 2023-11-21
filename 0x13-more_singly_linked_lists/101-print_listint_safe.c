2ebdab3fb63fe6e058d4611bbfd10d75f0ad4724
andy <91167584+andymutale@users.noreply.github.com>
2023-11-22 00:39:31 +0200

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,36 +1,52 @@
 #include "lists.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * print_listint_safe - Prints a listint_t linked list safely.
- * @head: Pointer to the head of the list.
- *
- * Return: The number of nodes in the list.
+ * print_listint_safe - prints a listint_t linked list
+ * @head: pointer to the start of the list
+ * 
+ * Return: the number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    const listint_t *current = head, *checker = head;
-    size_t count = 0;
+    const listint_t *slow, *fast;
+    size_t nodes = 0;
 
-    while (current != NULL)
+    if (head == NULL)
     {
-        printf("[%p] %d\n", (void *)current, current->n);
-        count++;
-
-        current = current->next;
-
-        /* Check for a loop in the linked list */
-        if (current == checker)
-        {
-            printf("-> [%p] %d\n", (void *)current, current->n);
-            exit(98);
-        }
-
-        if (current != NULL)
-            current = current->next;
-        checker = checker->next;
+        exit(98);
     }
 
-    return count;
+    slow = head;
+    fast = head;
+
+    while (slow != NULL && fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+        {
+            slow = head;
+            while (slow != fast)
+            {
+                printf("[%p] %d\n", (void *)slow, slow->n);
+                slow = slow->next;
+                nodes++;
+            }
+            printf("[%p] %d\n", (void *)slow, slow->n);
+            nodes++;
+            return (nodes);
+        }
+    }
+
+    slow = head;
+    while (slow != NULL)
+    {
+        printf("[%p] %d\n", (void *)slow, slow->n);
+        slow = slow->next;
+        nodes++;
+    }
+
+    return (nodes);
 }
