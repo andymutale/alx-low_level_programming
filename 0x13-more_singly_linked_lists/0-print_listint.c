51aa615d0b3b3d12fd2388595d85c48f7615e559
andy <91167584+andymutale@users.noreply.github.com>
2023-11-22 01:18:54 +0200

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,17 +9,17 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    const listint_t *current;
-    size_t count = 0;
+	const listint_t *current;
+	size_t count = 0;
 
-    current = h;
+	current = h;
 
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-        count++;
-    }
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		count++;
+	}
 
-    return (count);
+	return (count);
 }
