30ccaf11def04d002fe3208046d8f0c2d84a3bd5
andymutale <drewmutale@gmail.com>
2023-11-21 13:56:32 -0800

    m

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -13,6 +13,11 @@ typedef struct listint_s
 
 /* Function prototypes for list manipulation */
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void print_listint(const listint_t *h);
+size_t print_listint(const listint_t *h);
+
 listint_t *add_nodeint_end(listint_t **head, int n);
 void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
