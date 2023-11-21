7cd557a08f89b7602c1a1d8b3453a45b4349cc79
andy <91167584+andymutale@users.noreply.github.com>
2023-11-22 00:50:07 +0200

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,24 +1,26 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>
 
-/* Struct for a singly linked list node */
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct listint_s
 {
-    int n;                      /* Data of the node */
-    struct listint_s *next;     /* Pointer to the next node */
+    int n;
+    struct listint_s *next;
 } listint_t;
 
-/* Function prototypes for list manipulation */
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
-void print_listint(const listint_t *h);
-size_t print_listint(const listint_t *h);
-
-listint_t *add_nodeint_end(listint_t **head, int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
