a537300cbba05ca9e39b73ece7436e4f958cfba1
andy <91167584+andymutale@users.noreply.github.com>
2023-11-22 00:25:47 +0200

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,52 +1,52 @@
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * insert_nodeint_at_index - Inserts a new node at a given position.
- * @head: Pointer to the pointer of the head of the list
- * @idx: Index of the list where the new node should be added
- * @n: Value to be assigned to the new node
- *
- * Return: The address of the new node, or NULL if it failed
- *         (if it is not possible to add the new node at index idx)
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: double pointer to the start of the list
+ * @idx: index of the list where the new node should be added, starts at 0
+ * @n: data for the new node
+ * 
+ * Return: address of the new node, or NULL if it failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    listint_t *new_node, *temp;
-    unsigned int i = 0;
+    listint_t *new_node;
+    listint_t *current;
+    unsigned int count = 0;
 
     if (head == NULL)
+    {
         return (NULL);
+    }
+
+    new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL)
+    {
+        return (NULL);
+    }
+    new_node->n = n;
 
     if (idx == 0)
     {
-        new_node = malloc(sizeof(listint_t));
-        if (new_node == NULL)
-            return (NULL);
-
-        new_node->n = n;
         new_node->next = *head;
         *head = new_node;
         return (new_node);
     }
 
-    temp = *head;
-    while (temp != NULL && i < idx - 1)
+    current = *head;
+    while (current != NULL)
     {
-        temp = temp->next;
-        i++;
+        if (count == idx - 1)
+        {
+            new_node->next = current->next;
+            current->next = new_node;
+            return (new_node);
+        }
+        count++;
+        current = current->next;
     }
 
-    if (temp == NULL)
-        return (NULL);
-
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-        return (NULL);
-
-    new_node->n = n;
-    new_node->next = temp->next;
-    temp->next = new_node;
-
-    return (new_node);
+    free(new_node);
+    return (NULL);
 }
