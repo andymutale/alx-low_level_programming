92f3d3d67d66602811b390879cd1f1af875fb056
andy <91167584+andymutale@users.noreply.github.com>
2023-11-21 23:16:03 +0200

    Create 9-insert_nodeint.c

---
+++ 9-insert_nodeint.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - Inserts a new node at a given position.
+ * @head: Pointer to the pointer of the head of the list
+ * @idx: Index of the list where the new node should be added
+ * @n: Value to be assigned to the new node
+ *
+ * Return: The address of the new node, or NULL if it failed
+ *         (if it is not possible to add the new node at index idx)
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+    listint_t *new_node, *temp;
+    unsigned int i = 0;
+
+    if (head == NULL)
+        return (NULL);
+
+    if (idx == 0)
+    {
+        new_node = malloc(sizeof(listint_t));
+        if (new_node == NULL)
+            return (NULL);
+
+        new_node->n = n;
+        new_node->next = *head;
+        *head = new_node;
+        return (new_node);
+    }
+
+    temp = *head;
+    while (temp != NULL && i < idx - 1)
+    {
+        temp = temp->next;
+        i++;
+    }
+
+    if (temp == NULL)
+        return (NULL);
+
+    new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL)
+        return (NULL);
+
+    new_node->n = n;
+    new_node->next = temp->next;
+    temp->next = new_node;
+
+    return (new_node);
+}
