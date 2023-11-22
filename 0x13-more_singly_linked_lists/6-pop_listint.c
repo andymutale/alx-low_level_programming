9e234bc9317aaff3a4ed0f34d816f1e10ef351bd
andy <91167584+andymutale@users.noreply.github.com>
2023-11-22 03:28:47 +0200

    Update 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,18 +10,18 @@
  */
 int pop_listint(listint_t **head)
 {
-    listint_t *current;
-    int n;
+	listint_t *current;
+	int n;
 
-    if (*head == NULL)
-    {
-        return (0);
-    }
+	if (*head == NULL)
+	{
+		return (0);
+	}
 
-    current = *head;
-    *head = (*head)->next;
-    n = current->n;
-    free(current);
+	current = *head;
+	*head = (*head)->next;
+	n = current->n;
+	free(current);
 
-    return (n);
+	return (n);
 }
