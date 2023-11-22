24ac55faaee3034f918bb87d95514fd9a174c295
andy <91167584+andymutale@users.noreply.github.com>
2023-11-22 03:58:24 +0200

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,44 +10,44 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    const listint_t *slow, *fast;
-    size_t nodes = 0;
+	const listint_t *slow, *fast;
+	size_t nodes = 0;
 
-    if (head == NULL)
-    {
-        exit(98);
-    }
+	if (head == NULL)
+	{
+		exit(98);
+	}
 
-    slow = head;
-    fast = head;
+	slow = head;
+	fast = head;
 
-    while (slow != NULL && fast != NULL && fast->next != NULL)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
 
-        if (slow == fast)
-        {
-            slow = head;
-            while (slow != fast)
-            {
-                printf("[%p] %d\n", (void *)slow, slow->n);
-                slow = slow->next;
-                nodes++;
-            }
-            printf("[%p] %d\n", (void *)slow, slow->n);
-            nodes++;
-            return (nodes);
-        }
-    }
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				printf("[%p] %d\n", (void *)slow, slow->n);
+				slow = slow->next;
+				nodes++;
+			}
+			printf("[%p] %d\n", (void *)slow, slow->n);
+			nodes++;
+			return (nodes);
+		}
+	}
 
-    slow = head;
-    while (slow != NULL)
-    {
-        printf("[%p] %d\n", (void *)slow, slow->n);
-        slow = slow->next;
-        nodes++;
-    }
+	slow = head;
+	while (slow != NULL)
+	{
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		slow = slow->next;
+		nodes++;
+	}
 
-    return (nodes);
+	return (nodes);
 }
